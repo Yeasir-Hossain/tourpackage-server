cf403f61e9fc89dce4bb01db40d041318603f45a
Yeasir-Hossain <yeasir06@gmail.com>
2023-03-16 04:55:05 +0600

    server done

--- index.js
+++ index.js
@@ -7,26 +7,12 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors()) 
 app.use(express.json())
+
 
 const uri = `${process.env.MONGO_DB_URL}`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// function verifyJWT(req, res, next) {
-//     const authorization = req.headers.authorization;
-//     if (!authorization) {
-//         return res.status(401).send({ message: "UnAuthorized access" })
-//     }
-//     const token = authorization.split(' ')[1]
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             return res.status(403).send({ message: "Forbidden access" })
-//         }
-//         req.decoded = decoded
-//         next()
-//     });
-// }
 
 async function run() {
     try {
